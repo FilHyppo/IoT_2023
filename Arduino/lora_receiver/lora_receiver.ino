fa3ff3b78262ba5b0f516c80f326046e95209682
filippobologna <filippo@fam.bologna.it>
2024-01-31 17:10:58 +0100

    sender e receiver comunicano correttamente e il sender misura l'umidità

--- Arduino/lora_receiver/lora_receiver.ino
+++ Arduino/lora_receiver/lora_receiver.ino
@@ -1,33 +1,60 @@
 //arduino nero receiver
+
+// valori sul 500 indicano terreno  bagnato (acqua = 412), sul 700 indicano terreno secco (aria = 800) e per valori più alti indicano terreno via via più secco
 #include <SPI.h>
 #include <LoRa.h>
 
+
+
+unsigned long previousMillis = 0;  // Utilizzata per memorizzare il tempo del ciclo precedente
+const long interval = 2000;        // Intervallo di tempo tra un ciclo e l'altro, cambiando questo valore regolo il delay
+
+
 void setup() {
   Serial.begin(9600);
+  pinMode(13, OUTPUT);
   while (!Serial);
 
   Serial.println("LoRa Receiver");
 
-  if (!LoRa.begin(915E6)) {
+  if (!LoRa.begin(868E6)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
 }
 
 void loop() {
-  // try to parse packet
+
+  int umidita;
+  unsigned int id;
+
   int packetSize = LoRa.parsePacket();
+    // 1: read the input
+    
   if (packetSize) {
     // received a packet
-    Serial.print("Received packet '");
+    Serial.print("Received packet ");
 
     // read packet
     while (LoRa.available()) {
-      Serial.print((char)LoRa.read());
+      id = LoRa.parseInt();
+      umidita = LoRa.parseInt();
+      //stampa valori letti
+      Serial.print("id: ");
+      Serial.println(id);
+      Serial.print(" umidita: ");
+      Serial.println(umidita);
+      
     }
+    // 2: find the future state
 
-    // print RSSI of packet
-    Serial.print("' with RSSI ");
-    Serial.println(LoRa.packetRssi());
+    // 3: do the transition (i.e., change the state)
+
+    // 4: update the output
+
+    // 5: comunicazione seriale con bridge
+
+    String messaggio = String(id) + " " + String(umidita);
+    Serial.println(messaggio);  
   }
 }
--- Arduino/lora_sender/lora_sender.ino
+++ Arduino/lora_sender/lora_sender.ino
@@ -1,32 +1,58 @@
 //arduino verde sender
+// valori sul 500 indicano terreno  bagnato (acqua = 412), sul 700 indicano terreno secco (aria = 800) e per valori più alti indicano terreno via via più secco
+
 #include <SPI.h>
 #include <LoRa.h>
 
-int counter = 0;
+const unsigned int id = 4000;
+int count = 0;
+unsigned long previousMillis = 0;  // Utilizzata per memorizzare il tempo del ciclo precedente
+const long interval = 2000;        // Intervallo di tempo tra un ciclo e l'altro, cambiando questo valore regolo il delay
+
 
 void setup() {
   Serial.begin(9600);
+  pinMode(13, OUTPUT);
   while (!Serial);
 
   Serial.println("LoRa Sender");
 
-  if (!LoRa.begin(915E6)) {
+  if (!LoRa.begin(868E6)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
 }
 
 void loop() {
-  Serial.print("Sending packet: ");
-  Serial.println(counter);
+  unsigned long currentMillis = millis();  // Memorizza il tempo corrente
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;  // Memorizza il tempo corrente
+    // 1: read the input
+    int umidita;
+    umidita = analogRead(A0);
+    
+    // 2: find the future state
 
-  // send packet
-  LoRa.beginPacket();
-  LoRa.print("ciao ");
-  LoRa.print(counter);
-  LoRa.endPacket();
+    // 3: do the transition (i.e., change the state)
 
-  counter++;
+    // 4: update the output
 
-  delay(1000);
+    count++;
+
+    //stampe varie
+    Serial.print("Sending packet: ");
+    Serial.println(count);
+    Serial.print(" umidita: ");
+    Serial.println(umidita);
+    Serial.print(" id: ");
+    Serial.println(id);
+    
+
+    //send packet 
+    LoRa.beginPacket();
+    LoRa.print(id);
+    LoRa.print(".");  //serve per il parse int del receiver perchè legge tutti i byte che sono numeri quindi col . spezzo i valori
+    LoRa.print(umidita);
+    LoRa.endPacket();
+  }
 }
