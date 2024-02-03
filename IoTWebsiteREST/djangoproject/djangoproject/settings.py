e6d5c6825215034b4a6c6773dc71246fbf24b690
Morri02 <82643633+Morri02@users.noreply.github.com>
2024-02-03 10:30:39 +0100

    trigger MQTT
    Aggiunta funzione per invio messaggi MQTT e per triggerare irrigatore con messaggio ON

--- IoTWebsiteREST/djangoproject/djangoproject/settings.py
+++ IoTWebsiteREST/djangoproject/djangoproject/settings.py
@@ -37,8 +37,12 @@ SECRET_KEY = get_secret('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["postgredjango.ew.r.appspot.com", "localhost",
-                 "127.0.0.1"]
+
+ALLOWED_HOSTS = ["postgredjango.ew.r.appspot.com" ,
+                 'localhost',
+                 "127.0.0.1",
+                 '192.168.0.65']
+
 
 
 # Application definition
