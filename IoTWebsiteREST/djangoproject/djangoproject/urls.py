aa73efca77a074f2b14a3d4fe0cf57a6d83046ee
LorenzoCorradi <82643787+LorenzoCorradi@users.noreply.github.com>
2024-02-19 11:25:06 +0100

    https, favicon

--- IoTWebsiteREST/djangoproject/djangoproject/urls.py
+++ IoTWebsiteREST/djangoproject/djangoproject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include ,re_path
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,6 +25,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('mqtt/', include('mqtt_integration.urls')),
     path('', include('website.urls')),
+    re_path(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/logo.png')),
     #path('api-auth/', include('rest_framework.urls')),
     #path('api/rest-auth/', include('rest_auth.urls')),
     #path('api/rest-auth/registration/', include('rest_auth.registration.urls')),
